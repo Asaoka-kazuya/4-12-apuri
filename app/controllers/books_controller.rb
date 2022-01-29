ecab6f008a1236139773adbcf79e27cd4e37314d
Asaoka-kazuya <ookami19970418@gmail.com>
2022-01-29 10:27:52 +0000

    [finish]4-12

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,59 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+
+  def index
+    @book = Book.all
+    @book_new = Book.new
+  end
+
+  def create
+      @book = Book.all
+      @book_new = Book.new(book_params)
+
+       if @book_new.save
+         flash[:notice] = "Book was successfully created."
+         redirect_to book_path(@book_new.id)
+       else
+          render :index
+       end
+  end
+
+
+   def show
+     @book = Book.find(params[:id])
+   end
+
+   def edit
+    @book = Book.find(params[:id])
+   end
+
+   def update
+     @book = Book.find(params[:id])
+     if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+     else
+       render :edit
+     end
+
+
+   end
+
+   def destroy
+     @book = Book.find(params[:id])
+     if@book.destroy
+       flash[:notice] = "Book was successfully destroyed."
+     end
+     redirect_to '/books'
+   end
+
+
+
+   private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+end
